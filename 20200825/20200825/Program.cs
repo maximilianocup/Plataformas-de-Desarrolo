1c7e9e4bb6d96ebf7b9e29d10f7dade50b49538d
maximilianocup <cupersmidd@gmail.com>
2020-08-25 23:11:41 -0300

    ejercicio While

---
+++ 20200825/20200825/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _20200825
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 1;
+            string numero = "";
+            Console.WriteLine("Ingrese un numero");
+            //numero = Console.ReadLine();
+            while (numero != "3" && intentos <=3){
+                //Console.Write("Ingrese un numero");
+                numero = Console.ReadLine();
+                int.Parse(numero);
+                
+                if (numero == "3")
+                {
+                    Console.WriteLine("Felicitaciones");
+                }
+                else
+                {
+                    Console.WriteLine("siga intentando");
+                    Console.WriteLine("intento numero: " + intentos++);
+                }
+                
+
+            }
+
+            
+
+
+
+
+
+        }
+    }
+}
