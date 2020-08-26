389619c75ffa566ddd1a6917c68c570a23f5764d
maximilianocup <cupersmidd@gmail.com>
2020-08-25 21:22:47 -0300

    ejercicios 20200818

---
+++ 20200818/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tipee un numero del 0 al 15");
+            int numero;
+            string mensaje;
+            string valor = Console.ReadLine();
+            numero = int.Parse(valor);
+
+            if (numero < 1 || numero == 6 )
+            {
+                mensaje = "pusiste un 0 o un 6";
+                
+            }
+            else if (numero == 2)
+            {
+                mensaje = "pusiste el numero 2";
+                
+            }
+            else if (numero > 3 && numero < 8)
+            {
+                mensaje = "el numero esta entre el 3 y el 8";
+
+            }
+            else if (numero > 10 && numero < 15)
+            {
+                mensaje = "pusiste un numero de 2 digitos";
+
+
+            }
+            else 
+            {
+                mensaje = "el numero que puso no es valido";
+            }
+            
+            
+             Console.WriteLine(mensaje);
+            
+
+            
+
+        }
+    }
+}
---
+++ 20200818/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int sum = 0;
+            Console.WriteLine("ingrese 10 numeros, cuando la suma llegue a 50 se detendra");
+            
+            
+            
+            for ( int posicion = 0; posicion < 10; posicion++)
+            {
+                Console.WriteLine("ingrese el numero");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (sum > 50)
+                {
+                    break;
+                }
+
+                Console.WriteLine("el total sumado es: " + sum);
+
+            }
+        }
+    }
+}
